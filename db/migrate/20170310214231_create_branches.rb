3f556200913c2559a5d3648d87b412cb9dd1f1bd
Sean Clarke <xseanclarke@gmail.com>
2017-04-18 21:42:30 -0400

    minor indentation fix

--- db/migrate/20170310214231_create_branches.rb
+++ db/migrate/20170310214231_create_branches.rb
@@ -1,12 +1,12 @@
 class CreateBranches < ActiveRecord::Migration
   def change
     create_table :branches do |t|
-		t.string :event
-		t.string :organization
-		t.date :date
-		t.string :location
-		t.string :info
-     	t.integer :user_id
+	t.string :event
+	t.string :organization
+	t.date :date
+	t.string :location
+	t.string :info
+	t.integer :user_id
   end
 end
 end
